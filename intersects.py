44a1316ce735d8d5f42495ab4ba3d9d8368a4ce1
Grace Ryall <grace.ryall@bristol.ac.uk>
2024-05-13 16:41:55 +0100

    created get_ENSEMBL_ID function

--- intersects.py
+++ intersects.py
@@ -5,6 +5,9 @@ import pandas as pd
 UTR_coords = pd.read_csv('3UTR.csv', header = None, sep = '\t')
 #Now give columns names. First three columns comprise coords- give same header names as for DDX6 binding site table.
 UTR_coords.columns = ["chrom", "start", "end", "identifier", "unknown", "strand"]
+#create a new identifier column with useful values (i.e. only the ENSEMBL transcript identifier)
+UTR_coords['ENSEMBL_ID'] = UTR_coords['identifier'].str.split('_').str[0]
+print(UTR_coords.head())
 #We only want the first three columns.
 UTR_coords_subset = UTR_coords[["chrom", "start", "end"]]
 #The first three columns are named as such so that the dframe is compatible with bioframe
@@ -44,14 +47,22 @@ print(len(overlapping_intervals.index))
 #May be easiest to create a custom function for this.
 
 #May need to drop first three columns from overlapping_intervals for the function to work; the first three
-#columns correspond to 3'UTR coordinates from across the genome. 
+#columns correspond to 3'UTR coordinates from across the genome. Still need to retain first three columns for additional
+#data filtering.
 
-overlapping_intervals.drop(columns=['chrom_1', 'start_1', 'end_1'], inplace = True)
+overlapping_intervals = overlapping_intervals.drop_duplicates(subset=['chrom_2', 'start_2', 'end_2'], keep=False)
+DDX6_overlapping_intervals = overlapping_intervals.filter(regex='_2')  
+UTR_overlapping_intervals = overlapping_intervals.filter(regex='_1')
+
 #try re-naming the columns
-overlapping_intervals.rename({'chrom_2': 'chrom', 'start_2': 'start', 'end_2': 'end'}, axis=1, inplace=True)
-overlapping_intervals = overlapping_intervals.drop_duplicates(subset=['chrom', 'start', 'end'], keep=False)
-print(overlapping_intervals.head())
-print(len(overlapping_intervals.index))
+DDX6_overlapping_intervals = DDX6_overlapping_intervals.copy()
+UTR_overlapping_intervals = UTR_overlapping_intervals.copy()
+DDX6_overlapping_intervals.rename({'chrom_2': 'chrom', 'start_2': 'start', 'end_2': 'end'}, axis=1, inplace=True)
+UTR_overlapping_intervals.rename({'chrom_1': 'chrom', 'start_1': 'start', 'end_1': 'end'}, axis=1, inplace=True)
+print(DDX6_overlapping_intervals.head())
+print(len(DDX6_overlapping_intervals.index))
+print(UTR_overlapping_intervals.head())
+print(len(UTR_overlapping_intervals.index))
 #done! Seems to be 3201 3'UTR binding sites
 
 def get_additional_columns(row):
@@ -75,8 +86,30 @@ def get_additional_columns(row):
         return None
 
 # Apply get_additional_columns to overlapping_intervals
-additional_values_df = overlapping_intervals.apply(get_additional_columns, axis=1)
-overlapping_intervals[['strand', 'confidence_score', 'sample_or_tissue_used']] = additional_values_df.apply(pd.Series)
-print(overlapping_intervals.head())
+additional_values_df = DDX6_overlapping_intervals.apply(get_additional_columns, axis=1)
+DDX6_overlapping_intervals[['strand', 'confidence_score', 'sample_or_tissue_used']] = additional_values_df.apply(pd.Series)
+print(DDX6_overlapping_intervals.head())
 
 #it works! Now need to figure out how to get gene/transcript names from the coordinates
+#The easiest way to do this might be to take the identifier value in the original UTR database.
+#need to remove any value after '_' in the identifier- anything after this is superfluous- DONE
+#now need to make a new function that does the same as get_additional_columns, but with UTR_coords
+
+def get_ENSEMBL_ID (row):
+    coords = (row['chrom'], row['start'], row['end'])
+    matching_row = UTR_coords[
+        (UTR_coords['chrom'] == coords[0]) &
+        (UTR_coords['start'] == coords[1]) &
+        (UTR_coords['end'] == coords[2])
+    ]
+    if not matching_row.empty:
+        ENSEMBL_ID = matching_row['ENSEMBL_ID'].values[0]
+        return ENSEMBL_ID
+    else:
+        return None
+
+ensembl_values_df = UTR_overlapping_intervals.apply(get_ENSEMBL_ID, axis=1)
+UTR_overlapping_intervals['ENSEMBL_ID'] = ensembl_values_df.apply(pd.Series)
+print(UTR_overlapping_intervals.head())
+
+#The above works. Now need to append 'ENSEMBL_ID' column to DDX6_overlapping_intervals

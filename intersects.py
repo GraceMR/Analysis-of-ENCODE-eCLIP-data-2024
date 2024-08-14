e08f0577999656a3059d5284660ba54545868a1a
GRACE RYALL <grace.ryall@bristol.ac.uk>
2024-08-14 12:36:44 +0100

    incorporated sqlite3 into functions to handle larger dataframes

--- intersects.py
+++ intersects.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import bioframe as bf
 import numpy as np
+import sqlite3
 
 UTR_coords = pd.read_csv('3UTR.csv', header = None, sep = '\t')
 #Now give columns names. First three columns comprise coords- give same header names as for DDX6 binding site table.
@@ -78,65 +79,106 @@ print(len(DDX6_overlapping_intervals.index))
 UTR_overlapping_intervals.reset_index(drop=True, inplace=True)
 print(UTR_overlapping_intervals.head())
 print(len(UTR_overlapping_intervals.index))
-#done! Seems to be 3201 3'UTR binding sites
-#export this and look for Limk1 binding sites too- 74122298-74122353 and 74122308-74122367
+#done! Seems to be 39,024 3'UTR binding sites
+#confirmed limk1 3'UTR binding sites are present in both DDX6_overlapping_intervals and UTR_overlapping_intervals
 
-def get_additional_columns(row, DDX6_coords):
+##def get_additional_columns(row, DDX6_coords):
     #Ensure DDX6_coords has the correct index
-    DDX6_coords = DDX6_coords.set_index(['chrom', 'start', 'end'])
+    ##DDX6_coords = DDX6_coords.set_index(['chrom', 'start', 'end'])
     # Create a tuple for the index
-    coords_index = (row['chrom'], row['start'], row['end'])
+    ##coords_index = (row['chrom'], row['start'], row['end'])
 
     # Try to locate the matching row using the index
-    try:
-        matching_row = DDX6_coords.loc[coords_index]
-        strand = matching_row['strand']
-        confidence_score = matching_row['confidence_score']
-        sample_or_tissue_used = matching_row['sample_or_tissue_used']
-        additional_values = {
-            'strand': strand,
-            'confidence_score': confidence_score,
-            'sample_or_tissue_used': sample_or_tissue_used
-        }
-        return additional_values
-    except KeyError:
+    ##try:
+    ##    matching_row = DDX6_coords.loc[coords_index]
+        ##strand = matching_row['strand']
+        ##confidence_score = matching_row['confidence_score']
+        ##sample_or_tissue_used = matching_row['sample_or_tissue_used']
+        ##additional_values = {
+            ##'strand': strand,
+            ##'confidence_score': confidence_score,
+            ##'sample_or_tissue_used': sample_or_tissue_used
+        ##}
+        ##return additional_values
+    ##except KeyError:
         # If the coords_index is not found in DDX6_coords
-        return None
-
-
+        ##return None
 # Apply get_additional_columns to overlapping_intervals
-additional_values_df = DDX6_overlapping_intervals.apply(get_additional_columns, axis=1, args=(DDX6_coords,))
+#additional_values_df = DDX6_overlapping_intervals.apply(get_additional_columns, axis=1, args=(DDX6_coords,))
 
 # Convert the dictionaries in additional_values_df to a DataFrame
-additional_values_df = additional_values_df.apply(pd.Series)
+#additional_values_df = additional_values_df.apply(pd.Series)
 
 # Join the new columns to the original DDX6_overlapping_intervals DataFrame
-DDX6_overlapping_intervals = DDX6_overlapping_intervals.join(additional_values_df)
-print(DDX6_overlapping_intervals.head())
-print(len(DDX6_overlapping_intervals.index))
-print(DDX6_overlapping_intervals.columns)
+#DDX6_overlapping_intervals = DDX6_overlapping_intervals.join(additional_values_df)
+#print(DDX6_overlapping_intervals.head())
+#print(len(DDX6_overlapping_intervals.index))
+#print(DDX6_overlapping_intervals.columns)
+
+def process_with_sqlite(df, DDX6_coords):
+    conn = sqlite3.connect(':memory:')
+    df.to_sql('df', conn, index=False)
+    DDX6_coords.to_sql('DDX6_coords', conn, index=False)
+
+    # Verify the schema of the tables
+    print(pd.read_sql_query("PRAGMA table_info(df);", conn))
+    print(pd.read_sql_query("PRAGMA table_info(DDX6_coords);", conn))
+
+    query = """
+    SELECT df.*, DDX6_coords.strand, DDX6_coords.confidence_score, DDX6_coords.sample_or_tissue_used
+    FROM df
+    LEFT JOIN DDX6_coords
+    ON df.chrom = DDX6_coords.chrom AND df.start = DDX6_coords.start AND df.end = DDX6_coords.end
+    """
+    result = pd.read_sql_query(query, conn)
+    conn.close()
+    return result
+
+DDX6_3UTR_binding_sites = process_with_sqlite(DDX6_overlapping_intervals, DDX6_coords)
+print(DDX6_3UTR_binding_sites.head())
+print(len(DDX6_3UTR_binding_sites.index))
 
 #it works! Now need to figure out how to get gene/transcript names from the coordinates
 #The easiest way to do this might be to take the identifier value in the original UTR database.
 #need to remove any value after '_' in the identifier- anything after this is superfluous- DONE
 #now need to make a new function that does the same as get_additional_columns, but with UTR_coords
 
-def get_GENCODE_ID (row):
-    coords = (row['chrom'], row['start'], row['end'])
-    matching_row = UTR_coords[
-        (UTR_coords['chrom'] == coords[0]) &
-        (UTR_coords['start'] == coords[1]) &
-        (UTR_coords['end'] == coords[2])
-    ]
-    if not matching_row.empty:
-        GENCODE_ID = matching_row['GENCODE_ID'].values[0]
-        return GENCODE_ID
-    else:
-        return None
+#def get_GENCODE_ID (row):
+    ##coords = (row['chrom'], row['start'], row['end'])
+    ##matching_row = UTR_coords[
+        ##(UTR_coords['chrom'] == coords[0]) &
+        ##(UTR_coords['start'] == coords[1]) &
+        ##(UTR_coords['end'] == coords[2])
+    ##]
+    ##if not matching_row.empty:
+        ##GENCODE_ID = matching_row['GENCODE_ID'].values[0]
+        ##return GENCODE_ID
+    ##else:
+        ##return None
 
-gencode_values_df = UTR_overlapping_intervals.apply(get_GENCODE_ID, axis=1)
-UTR_overlapping_intervals['GENCODE_ID'] = gencode_values_df.apply(pd.Series)
-print(UTR_overlapping_intervals.head())
+##gencode_values_df = UTR_overlapping_intervals.apply(get_GENCODE_ID, axis=1)
+##UTR_overlapping_intervals['GENCODE_ID'] = gencode_values_df.apply(pd.Series)
+##print(UTR_overlapping_intervals.head())
+
+def get_GENCODE_ID_with_sqlite(df, UTR_coords):
+    conn = sqlite3.connect(':memory:')
+    df.to_sql('df', conn, index=False)
+    UTR_coords.to_sql('UTR_coords', conn, index=False)
+
+    query = """
+    SELECT df.*, UTR_coords.GENCODE_ID
+    FROM df
+    LEFT JOIN UTR_coords
+    ON df.chrom = UTR_coords.chrom AND df.start = UTR_coords.start AND df.end = UTR_coords.end
+    """
+    result = pd.read_sql_query(query, conn)
+    conn.close()
+    return result
+
+UTR_gencode_ids = get_GENCODE_ID_with_sqlite(UTR_overlapping_intervals, UTR_coords)
+print(UTR_gencode_ids)
+print(UTR_gencode_ids.head())
+print(len(UTR_gencode_ids.index))
 #DDX6_overlapping_intervals.to_csv('DDX6_overlapping_intervals.csv', index=True)
 #UTR_overlapping_intervals.to_csv('UTR_overlapping_intervals.csv', index=True)
 
